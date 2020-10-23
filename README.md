4b4a70944051c101b103afb3f149627145d0bba8
Zohair-coder <zohair.ul.hasan@gmail.com>
2020-10-23 05:18:24 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Caesar
+
+For more information, go to [CS50 pset2](https://cs50.harvard.edu/x/2020/psets/2/caesar/)
---
+++ test2.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+// Psuedo-code:
+// If the input is not a number or has more than one or no command line arguments then print error code. DONE
+// Convert string input to integer using atoi(). DONE
+// Input for plain text. DONE
+// Loop through each letter in plain text. DONE
+// Assign character a value in the alphabet e.g. B = 2 by subtracting ascii value of B. DONE
+// Use ci = (pi + k) % 26 to assign the new value to B
+// Save the new value of B in a new string
+
+int main(int argc, string argv[])
+{
+
+    if (argc == 2 && isdigit(*argv[1]))
+    {
+        int key = atoi(argv[1]);
+        string plaintext = get_string("plaintext: ");
+        char ciphertext[strlen(plaintext)];
+        char ref;
+        for (int i = 0, len = strlen(plaintext); i < len; i++)
+        {
+            if isupper(plaintext[i])
+            {
+                ref = 'A';
+            }
+            else if islower(plaintext[i])
+            {
+                ref = 'a';
+            }
+            else
+            {
+                ciphertext[i] = plaintext[i];
+                continue;
+            }
+            int rank = plaintext[i] - ref;
+            char new_rank = (rank + key) % 26 + ref;
+            ciphertext[i] = new_rank;
+        }
+        printf("ciphertext: %s\n", ciphertext);
+        return 0;
+    }
+    else
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+}
+
